b83bd41d61fd3cdfbca8ad54d240e9fcf0cc453e
Cory Johns <johnsca@gmail.com>
2021-06-03 13:10:16 -0400

    Install charm and charmcraft snaps (#9)
    * Install charm and charmcraft snaps
    
    Due to charmed-kubernetes/pytest-operator#13 we need to switch to using
    the snaps for building charms, and this needs to ensure that those snaps
    are available. Additionally, the strict confinement of the `charmcraft`
    snap combined with the fact that the runners provided by GitHub
    automatically include `git-lfs` means that we have to work around the
    /etc/gitconfig access issue.
    
    Fixes #8
    
    * Fix gitconfig cat issue

--- src/bootstrap/index.ts
+++ src/bootstrap/index.ts
@@ -59,6 +59,16 @@ async function run() {
             return
         }
 
+        await exec.exec("sudo snap install charm --classic");
+        await exec.exec("sudo snap install charmcraft");
+
+        // Workaround for strictly confined charmcraft not being able to access /etc/gitconfig.
+        // (Also, self-hosted runners might not have /etc/gitconfig, so this has to ignore errors.)
+        const options: exec.ExecOptions = {}
+        options.ignoreReturnCode = true;
+        await exec.exec("bash", ["-c", `cat /etc/gitconfig >> ${HOME}/.gitconfig`], options);
+        await exec.exec("sudo rm /etc/gitconfig", [], options);
+
         await exec.exec(bootstrap_command);
         core.exportVariable('CONTROLLER_NAME', controller_name);
     } catch(error) {
