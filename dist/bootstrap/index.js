58f6d35198948754974f2678b710643f1807ff44
Cory Johns <johnsca@gmail.com>
2021-08-12 10:42:49 -0400

    Add wait for storage to workaround for microk8s bootstrap race

--- dist/bootstrap/index.js
+++ dist/bootstrap/index.js
@@ -1633,8 +1633,9 @@ function run() {
                 yield exec.exec('bash', ['-c', 'sudo usermod -a -G microk8s $USER']);
                 yield exec.exec('sg microk8s -c "microk8s status --wait-ready"');
                 yield exec.exec('sg microk8s -c "microk8s enable storage dns rbac"');
-                // workaround for https://bugs.launchpad.net/juju/+bug/1937282
+                // workarounds for https://bugs.launchpad.net/juju/+bug/1937282
                 yield exec.exec('sg microk8s -c "microk8s kubectl wait --for=condition=available --timeout=5m -nkube-system deployment/coredns deployment/hostpath-provisioner"');
+                yield exec.exec('sg microk8s -c "microk8s kubectl rollout status deployment/hostpath-provisioner -n kube-system"');
                 bootstrap_command = `sg microk8s -c "${bootstrap_command}"`;
                 core.endGroup();
             }
