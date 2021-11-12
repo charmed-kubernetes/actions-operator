2099716fd2c54027b867fff55b010548432967d5
Andrew Scribner <ca.scribner+1@gmail.com>
2021-11-12 16:32:46 -0500

    fix mixup with juju/juju bundle

--- src/bootstrap/index.ts
+++ src/bootstrap/index.ts
@@ -54,6 +54,7 @@ async function run() {
     const charm_channel = core.getInput("charm_channel");
     const charmcraft_channel = core.getInput("charmcraft_channel");
     const juju_channel = core.getInput("juju_channel");
+    const juju_bundle_channel = core.getInput("juju_bundle_channel");
     let bootstrap_constraints = "cores=2 mem=4G";
     let group = "";
     try {
@@ -80,13 +81,13 @@ async function run() {
         await exec.exec("sudo --preserve-env=http_proxy,https_proxy,no_proxy pip3 install tox");
         core.endGroup();
         core.startGroup("Install Juju");
-        await exec.exec("sudo snap install juju --classic");
+        await exec.exec("sudo snap install juju --classic --channel=${juju_channel}");
         core.endGroup();
         core.startGroup("Install tools");
         await exec.exec("sudo snap install jq");
         await exec.exec("sudo snap install charm --classic --channel=${charm_channel}");
         await exec.exec("sudo snap install charmcraft --channel=${charmcraft_channel}");
-        await exec.exec("sudo snap install juju-bundle --channel=${juju_channel}");
+        await exec.exec("sudo snap install juju-bundle --channel=${juju_bundle_channel}");
         core.endGroup();
         let bootstrap_command = `juju bootstrap --debug --verbose ${provider} ${bootstrap_options}`
         if (provider === "lxd") {
