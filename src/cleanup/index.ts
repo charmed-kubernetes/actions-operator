bae3b47520c548dd19937ec31ec346d2487dc09d
Robert Gildein <robert.gildein@canonical.com>
2022-02-24 17:52:26 +0100

    Add juju-crashdump (#31)
    - install juju-crashdump
    - upload any juju-crashdumps to GitHub artifact

--- src/cleanup/index.ts
+++ src/cleanup/index.ts
@@ -1,5 +1,7 @@
+import * as artifact from '@actions/artifact';
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
+import * as glob from '@actions/glob';
 
 declare var process : {
     env: {
@@ -7,9 +9,23 @@ declare var process : {
     }
 }
 
+async function find_juju_crashdump(): Promise<string[]> {
+    const globber = await glob.create('**/juju-crashdump-*.tar.xz');
+    return globber.glob();
+}
+
+async function upload_artifact(files: string[]) {
+    const artifact_client = await artifact.create();
+    const result = await artifact_client.uploadArtifact(
+        "juju-crashdump", files, ".", {continueOnError: true}
+    );
+    core.info(`artifact ${result.artifactName} (${result.size}) was uploaded`);
+}
+
 async function run() {
     const controller_name = process.env["CONTROLLER_NAME"];
     const provider = core.getInput("provider");
+
     try {
         if (controller_name) {
             core.addPath('/snap/bin');
@@ -23,6 +39,17 @@ async function run() {
                 await exec.exec("juju remove-cloud microstack --client");
             }
         }
+
+        core.startGroup("uploading juju-crashdump");
+        const crash_dumps: string[] = await find_juju_crashdump();
+        if (crash_dumps.length > 0) {
+            core.info(`found juju-crashdump: ${crash_dumps}`);
+            await upload_artifact(crash_dumps);
+        } else {
+            core.info("no juju-crashdump was found")
+        }
+        core.endGroup();
+
     } catch(error) {
         core.setFailed(error.message);
     }
--- tests/test_deploy_machine.py
+++ tests/test_deploy_machine.py
@@ -3,5 +3,5 @@ import pytest
 
 @pytest.mark.abort_on_fail
 async def test_build(ops_test):
-    await ops_test.model.deploy("cs:ubuntu")
+    await ops_test.model.deploy("ch:ubuntu")
     await ops_test.model.wait_for_idle()
