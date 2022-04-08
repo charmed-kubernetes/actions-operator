f252c71d52715762fc5cd32b9aac4f2a52ea9e41
Adam Dyess <adam.dyess@canonical.com>
2022-04-08 10:31:29 -0500

    makes juju bootstrap-contraints a parameter of actions-operator

--- dist/bootstrap/index.js
+++ dist/bootstrap/index.js
@@ -1757,7 +1757,7 @@ function run() {
         const juju_bundle_channel = core.getInput("juju-bundle-channel");
         const juju_crashdump_channel = core.getInput("juju-crashdump-channel");
         const lxd_channel = core.getInput("lxd-channel");
-        let bootstrap_constraints = "cores=2 mem=4G";
+        let bootstrap_constraints = core.getInput("bootstrap-constraints");
         let group = "";
         try {
             core.addPath('/snap/bin');
