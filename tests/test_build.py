763973b79b50ec50279bf779e79449896f63b037
Adam Dyess <adam.dyess@canonical.com>
2022-05-17 10:27:40 -0500

    test on multiple versions of microk8s

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,15 +1,17 @@
 from subprocess import run
+import os
 import pytest
 
 
 @pytest.mark.abort_on_fail
 async def test_build(ops_test):
+    home_dir = os.environ.get("HOME") or "/tmp"
     run(
         [
             "git",
             "clone",
             "https://github.com/juju-solutions/charm-ubuntu.git",
-            "/home/ubuntu/charm-ubuntu",
+            f"{home_dir}/charm-ubuntu",
         ]
     )
-    await ops_test.build_charm("/home/ubuntu/charm-ubuntu")
+    await ops_test.build_charm(f"{home_dir}/charm-ubuntu")
