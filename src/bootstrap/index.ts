55834658f21e541f9a0dfc67257e51a50159de7e
Ghislain Bourgeois <ghislain.bourgeois@gmail.com>
2023-07-21 10:33:15 -0400

    Fix unnecessary delay in setting up microk8s

--- src/bootstrap/index.ts
+++ src/bootstrap/index.ts
@@ -106,7 +106,7 @@ async function microk8s_init(addons) {
     }
 
     await retry_until_rc("microk8s kubectl auth can-i create pods")
-    await retry_until_rc("microk8s kubectl auth can-i create pods --as=me", 1)
+    await retry_until_rc("microk8s kubectl auth can-i create pods --as=me")
     return true;
 }
 
